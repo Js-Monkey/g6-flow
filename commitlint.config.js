989fa088eb8fb01327b292db945f8af6e6b3f960
jumodada <864353360@qq.com>
2020-07-17 17:45:38 +0800

    chore: commitlint.config support 'update'

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional']
+const Configuration = {
+    extends: ['@commitlint/config-conventional'],
+    formatter: '@commitlint/format',
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor','perf', 'test', 'chore','deps']]
+    }
 }
+
+module.exports = Configuration
